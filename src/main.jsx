3c3c378c9184d1a9da8fe0d7d06ac17f1cff724c
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-04-17 17:07:55 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// ✅ ADD THESE
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+AOS.init({
+  duration: 1000,
+  once: true,
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
