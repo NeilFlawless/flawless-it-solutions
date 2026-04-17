ff3855afaf3baa68981b98dab2ed70f158777774
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-04-17 16:45:01 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 export default function Navbar() {
   return (
-    <nav className="fixed w-full bg-black/90 text-white px-8 py-4 flex justify-between">
+    <nav className="fixed top-0 left-0 w-full z-50 flex justify-between items-center px-8 py-4 bg-black/60 backdrop-blur-md">
       <h1 className="text-xl font-bold text-primary">
         Flawless IT Solutions
       </h1>
+
+      <div className="space-x-6 hidden md:flex">
+        <a href="#services" className="hover:text-primary">Services</a>
+        <a href="#about" className="hover:text-primary">About</a>
+        <a href="#contact" className="hover:text-primary">Contact</a>
+      </div>
     </nav>
   );
 }
