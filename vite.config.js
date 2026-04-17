224b5707cb1031011dc5360c151dbca024fb2ac7
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-04-17 16:03:09 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/flawless-it-solutions/', // 🔥 IMPORTANT
   plugins: [react()],
 })
